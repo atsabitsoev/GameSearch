e3bcb04fe32d8e31932ecb7bd06aaf434ec26be6
Ацамаз Бицоев <ios@bitsoev.ru>
2025-05-09 17:30:15 +0300

    Initial Commit

---
+++ GameSearch/GameSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameSearchApp.swift
+//  GameSearch
+//
+//  Created by Ацамаз on 09.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct GameSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
