c906b8e8b11488214dd822ca9c66e3b2ccda0bbc
EBabochiev <EBabochiev@spb.beeline.ru>
2025-05-13 19:47:02 +0300

    FullClubModel

---
+++ GameSearch/Models/Club/ClubDetailsData.swift
@@ -0,0 +1,35 @@
+//
+//  ClubDetailsData.swift
+//  GameSearch
+//
+//  Created by Бабочиев Эдуард Таймуразович on 13.05.2025.
+//
+
+struct ClubDetailsData {
+    init(club: FullClubData) {
+        self.id = club.id
+        self.name = club.name
+        self.rating = club.rating
+        self.description = club.description
+        self.image = club.image
+        self.prices = club.prices
+        self.promos = club.promos
+        self.additionalInfo = club.additionalInfo
+    }
+    
+    var id: Int
+    var name: String
+    var rating: Double
+    var description: String
+    var image: String
+    var prices: String
+    var promos: String
+    var additionalInfo: String
+}
+
+
+extension FullClubData {
+    func getDetailsData() -> ClubDetailsData {
+        ClubDetailsData(club: self)
+    }
+}
