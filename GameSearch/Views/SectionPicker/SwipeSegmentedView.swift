7eb05ded3ed4d8906701bcc48b74c923afd611ca
Ацамаз Бицоев <>
2025-05-22 18:51:38 +0300

    added padding to tabview

--- GameSearch/Views/SectionPicker/SwipeSegmentedView.swift
+++ GameSearch/Views/SectionPicker/SwipeSegmentedView.swift
@@ -33,6 +33,7 @@ struct SwipeSegmentedView<Content: View>: View {
                         .tag(segment)
                 }
             }
+            .padding(.horizontal, 16)
             .tabViewStyle(.page(indexDisplayMode: .never))
             .animation(.spring(duration: 0.3), value: selectedSegment)
         }
